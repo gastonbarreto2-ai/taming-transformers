60ca72af81c2008d4f5e1fef7148f3696caea167
gastonbarreto2-ai <gastonbarreto2@gmail.com>
2026-05-11 20:18:27 -0300

    Update vqgan.py

--- taming/models/vqgan.py
+++ taming/models/vqgan.py
@@ -42,7 +42,7 @@ class VQModel(pl.LightningModule):
             self.monitor = monitor
 
     def init_from_ckpt(self, path, ignore_keys=list()):
-        sd = torch.load(path, map_location="cpu")["state_dict"]
+        sd = torch.load(path, map_location="cpu" , weights_only=False)["state_dict"]
         keys = list(sd.keys())
         for k in keys:
             for ik in ignore_keys:
